b8fb8aa462530211b526585070f83e916fd1ddc7
pablokz1 <pablo.carvalho16@hotmail.com>
2023-10-24 23:29:03 -0300

    Inclusão de estrutura para persistência para platform utilizando sqlite3

---
+++ src/models/platform.js
@@ -0,0 +1,8 @@
+class Platform {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+module.exports = Platform;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userController = require('./controllers/user-controller');
+const platformController = require('./controllers/platform-controller');
 
 const routes = express();
 
@@ -8,5 +9,11 @@ routes.get('/users/:id', userController.getById);
 routes.post('/users', userController.post);
 routes.put('/users/:id', userController.putById);
 routes.delete('/users/:id', userController.deleteById);
+
+routes.get('/platforms', platformController.get);
+routes.get('/platforms/:id', platformController.getById);
+routes.post('/platforms', platformController.post);
+routes.put('/platforms/:id', platformController.putById);
+routes.delete('/platforms/:id', platformController.deleteById);
 
 module.exports = routes;
