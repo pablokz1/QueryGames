18fc8c7f8667569f0fb5be1a37bdbe4e419d2354
pablokz1 <pablo.carvalho16@hotmail.com>
2023-11-13 12:22:15 -0300

    Atualizado rotas e autorizações

--- src/routes.js
+++ src/routes.js
@@ -10,32 +10,32 @@ const routes = express();
 
 routes.post('/authentication', authenticationController.postAuth);
 
-routes.get('/users', authenticationController.verifyToken, userController.get);
-routes.get('/users/:id', authenticationController.verifyToken, userController.getById);
+routes.get('/users', userController.get);
+routes.get('/users/:id', userController.getById);
 routes.post('/users', authenticationController.verifyToken, userController.post);
 routes.put('/users/:id', authenticationController.verifyToken, userController.putById);
 routes.delete('/users/:id', authenticationController.verifyToken, userController.deleteById);
 
-routes.get('/platforms', authenticationController.verifyToken, platformController.get);
-routes.get('/platforms/:id', authenticationController.verifyToken, platformController.getById);
+routes.get('/platforms', platformController.get);
+routes.get('/platforms/:id', platformController.getById);
 routes.post('/platforms', authenticationController.verifyToken, platformController.post);
 routes.put('/platforms/:id', authenticationController.verifyToken, platformController.putById);
 routes.delete('/platforms/:id', authenticationController.verifyToken, platformController.deleteById);
 
-routes.get('/categories', authenticationController.verifyToken, categoryRepository.get);
-routes.get('/categories/:id', authenticationController.verifyToken, categoryRepository.getById);
+routes.get('/categories', categoryRepository.get);
+routes.get('/categories/:id', categoryRepository.getById);
 routes.post('/categories', authenticationController.verifyToken, categoryRepository.post);
 routes.put('/categories/:id', authenticationController.verifyToken, categoryRepository.putById);
 routes.delete('/categories/:id', authenticationController.verifyToken, categoryRepository.deleteById);
 
-routes.get('/games', authenticationController.verifyToken, gameRepository.get);
-routes.get('/games/:id', authenticationController.verifyToken, gameRepository.getById);
+routes.get('/games', gameRepository.get);
+routes.get('/games/:id', gameRepository.getById);
 routes.post('/games', authenticationController.verifyToken, gameRepository.post);
 routes.put('/games/:id', authenticationController.verifyToken, gameRepository.putById);
 routes.delete('/games/:id', authenticationController.verifyToken, gameRepository.deleteById);
 
-routes.get('/scores', authenticationController.verifyToken, scoreRepository.get);
-routes.get('/scores/:id', authenticationController.verifyToken, scoreRepository.getById);
+routes.get('/scores', scoreRepository.get);
+routes.get('/scores/:id', scoreRepository.getById);
 routes.post('/scores', authenticationController.verifyToken, scoreRepository.post);
 routes.put('/scores/:id', authenticationController.verifyToken, scoreRepository.putById);
 routes.delete('/scores/:id', authenticationController.verifyToken, scoreRepository.deleteById);
